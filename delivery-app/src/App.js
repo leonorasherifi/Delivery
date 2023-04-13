60e750df06e2acdb8c9021264fe015a27ed6eef4
Gentiana-Idrizi <gentiana.idrizi@student.uni-pr.edu>
2023-04-14 01:05:45 +0200

    Update App.js

--- delivery-app/src/App.js
+++ delivery-app/src/App.js
@@ -1,33 +1,17 @@
 
 import { Component } from 'react';
 import './App.css';
+import Header from './component/Header';
 
-class App extends Component{
 
-  constructor(props){
-    super(props);
-    this.state = {
-      users: []
-    }
-  }
-  componentDidMount(){
-    fetch('http://localhost:3000/users')
-    .then(response => response.json())
-    .then(res =>{
-      if(res.data){
-        console.log(res.data)
-      }
-    });
-  }
 
-  render(){  
-    return (
-      <div className="App">
-        <h1>Delivery app...</h1>
-      </div>
-    );
-  }
+
+function App() {
+return(
+  <div>
+    <Header/>
+  </div>
+  );
 }
-
 
 export default App;
