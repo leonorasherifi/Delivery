dac660489fd73244a0f0fab8a61b7d8dc2f32d28
Gentiana-Idrizi <gentiana.idrizi@student.uni-pr.edu>
2023-04-14 01:37:43 +0200

    Update Header.js

--- delivery-app/src/component/Header.js
+++ delivery-app/src/component/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link component from react-router-dom
 import logo from '../assest/logo.png';
+import { BsCartFill } from "react-icons/bs";
+import { FaUserAlt } from "react-icons/fa";
+
 
 export const Header = () => {
   return (
@@ -11,6 +14,24 @@ export const Header = () => {
             <img src={logo} className="h-full" />
           </div>
         </Link>
+
+        <div className='flex items-center gap-4 md:gap-7'>
+<nav className='gap-4 md:gap-6 text-base md:text-lg hidden md:flex'>
+
+            <Link to={""}>Home</Link>
+            <Link to={"menu/63f0fdbb3bcc2f97fa53d25d"}>Menu</Link>
+            <Link to={"about"}>About</Link>
+            <Link to={"contact"}>Contact</Link>
+
+</nav>
+<div className=''>
+  <BsCartFill/>
+</div>
+<div className=''>
+  <FaUserAlt/>
+</div>
+        </div>
+        
       </div>
     </header>
   );
