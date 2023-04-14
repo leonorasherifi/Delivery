0ff026b063a11c30c69baea549e6e4f7ecf90e73
Gentiana-Idrizi <gentiana.idrizi@student.uni-pr.edu>
2023-04-14 02:14:07 +0200

    Update Header.js

--- delivery-app/src/component/Header.js
+++ delivery-app/src/component/Header.js
@@ -19,7 +19,7 @@ export const Header = () => {
 <nav className='gap-4 md:gap-6 text-base md:text-lg hidden md:flex'>
 
             <Link to={""}>Home</Link>
-            <Link to={"menu/63f0fdbb3bcc2f97fa53d25d"}>Menu</Link>
+            <Link to={"menu"}>Menu</Link>
             <Link to={"about"}>About</Link>
             <Link to={"contact"}>Contact</Link>
 
