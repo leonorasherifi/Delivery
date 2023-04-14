119fa11b7525c01d71e8c9575b5bebe80653e69c
Gentiana-Idrizi <gentiana.idrizi@student.uni-pr.edu>
2023-04-15 01:30:13 +0200

    Update index.js

--- delivery-app/src/index.js
+++ delivery-app/src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {createBrowserRouter, createRoutesElements,createRoutesFromElements,Route,RouterProvider } from 'react-router-dom'
-
+import Home from "./page/Home";
+import Menu from "./page/Menu";
+import About from "./page/About";
+import Contact from "./page/Contact";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App/>}>
+    <Route index element={<Home/>}/>
+    <Route path="menu" element={<Menu />} />
+  <Route path="about" element={<About />} />
+  <Route path="contact" element={<Contact />} />
 
     </Route>
   )
@@ -16,6 +23,7 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <RouterProvider router={router}/>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
