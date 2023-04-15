6fe3bd29b173712891a7a44d3d91f403af9c89f2
Gentiana-Idrizi <gentiana.idrizi@student.uni-pr.edu>
2023-04-15 03:04:37 +0200

    Update Signup.js

--- delivery-app/src/page/Signup.js
+++ delivery-app/src/page/Signup.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import loginSignupImage from "../assest/login-animation.gif";
+
 
 function Signup() {
   return (
-    <div className=''>
 
+    <div className="p-3 md:p-4">
+      <div className="w-full max-w-sm bg-white m-auto flex  flex-col p-4">
+      <img src={loginSignupImage} />
+    <div className=''>
+    </div>
+    </div>
     </div>
   )
 }
