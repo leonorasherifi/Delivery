7c835264cfdd60600f5fbb03d7e5374360d359ab
gentianamejzini <gentiana.mejzini@student.uni-pr.edu>
2023-05-18 21:36:11 +0200

    Update App.js

--- delivery-app/src/App.js
+++ delivery-app/src/App.js
@@ -2,7 +2,7 @@
 import { Component } from 'react';
 import './App.css';
 import Header from './component/Header';
-
+import {Outlet} from 'react-router-dom';
 
 
 
@@ -12,7 +12,7 @@ return(
   <div>
     <Header/>
     <main className='pt-1 bg-slate-200 min-h-[calc(100vh)]'>
- 
+    <Outlet/>
     </main>
   </div>
   );
