afb0f3cd3a787b1d999e2d21563dd27aba7919bd
Gentiana-Idrizi <gentiana.idrizi@student.uni-pr.edu>
2023-05-22 23:06:38 +0200

    Update App.js

--- delivery-app/src/App.js
+++ delivery-app/src/App.js
@@ -3,18 +3,25 @@ import { Component } from 'react';
 import './App.css';
 import Header from './component/Header';
 import {Outlet} from 'react-router-dom';
+import toast, { Toaster } from "react-hot-toast";
+
 
 
 
 
 function App() {
 return(
+  <>
+  <Toaster/>
+
   <div>
+
     <Header/>
     <main className='pt-1 bg-slate-200 min-h-[calc(100vh)]'>
     <Outlet/>
     </main>
   </div>
+  </>
   );
 }
 
