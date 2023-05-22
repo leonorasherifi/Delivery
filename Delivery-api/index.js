2959d7f01c79738f259b025a190fdc0d198e7803
Gentiana-Idrizi <gentiana.idrizi@student.uni-pr.edu>
2023-05-22 23:07:17 +0200

    Update index.js

--- Delivery-api/index.js
+++ Delivery-api/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
 
 const app = express();
 app.use(cors());
@@ -8,6 +10,29 @@ app.use(express.json({ limit: "10mb" }));
 
 
 const PORT = process.env.PORT || 8080;
+
+//mongodb connection
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connect to Databse"))
+  .catch((err) => console.log(err));
+
+  //schema
+const userSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: {
+      type: String,
+      unique: true,
+    },
+    password: String,
+    confirmPassword: String,
+    image: String,
+  });
+
+//
+const userModel = mongoose.model("user", userSchema);
 
 //api
 app.get("/", (req, res) => {
@@ -18,7 +43,47 @@ app.get("/", (req, res) => {
 app.post("/signup", async (req, res) => {
     // console.log(req.body);
     const { email } = req.body;
-});
+  
+    userModel.findOne({ email: email }, (err, result) => {
+      // console.log(result);
+      console.log(err);
+      if (result) {
+        res.send({ message: "Email id is already register", alert: false });
+      } else {
+        const data = userModel(req.body);
+        const save = data.save();
+        res.send({ message: "Successfully sign up", alert: true });
+      }
+    });
+  });
+
+//api login
+app.post("/login", (req, res) => {
+    // console.log(req.body);
+    const { email } = req.body;
+    userModel.findOne({ email: email }, (err, result) => {
+      if (result) {
+        const dataSend = {
+          _id: result._id,
+          firstName: result.firstName,
+          lastName: result.lastName,
+          email: result.email,
+          image: result.image,
+        };
+        console.log(dataSend);
+        res.send({
+          message: "Login is successfully",
+          alert: true,
+          data: dataSend,
+        });
+      } else {
+        res.send({
+          message: "Email is not available, please sign up",
+          alert: false,
+        });
+      }
+    });
+  });
 
   //server is ruuning
 app.listen(PORT, () => console.log("server is running at port : " + PORT));
