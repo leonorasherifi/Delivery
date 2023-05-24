64260f243f942d054bdebb91874962802431113e
Gentiana-Idrizi <gentiana.idrizi@student.uni-pr.edu>
2023-05-24 02:14:34 +0200

    Update App.js

--- delivery-app/src/App.js
+++ delivery-app/src/App.js
@@ -28,7 +28,7 @@ return(
   <div>
 
     <Header/>
-    <main className='pt-1 bg-slate-200 min-h-[calc(100vh)]'>
+    <main className='pt-10 bg-slate-200 min-h-[calc(100vh)]'>
     <Outlet/>
     </main>
   </div>
