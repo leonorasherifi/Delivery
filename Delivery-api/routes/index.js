79597641a7ba3187593a45b9da0ef6b8c1e1a827
Gentiana-Idrizi <gentiana.idrizi@student.uni-pr.edu>
2023-05-24 23:57:17 +0200

    Update index.js

--- Delivery-api/routes/index.js
+++ Delivery-api/routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
-module.exports = router;
+// module.exports = router;
+
+// const { PingController } = require("../controllers/PingController");
+// const endpoints = [["/api/ping", PingController]];
+
+// export const routes = (app) => {
+//   [...swaggerEndpoints, ...endpoints].forEach((route) => {
+//     const [url, controller] = route;
+//     app.use(url, controller);
+//   });
+// };
+
+const PingController = require("../controllers/PingController");
+const ProductController = require("../controllers/ProductController");
+const AuthController = require("../controllers/AuthController");
+
+const routes = (app) => {
+  app.use("/ping", PingController);
+  app.use("/product", ProductController);
+  app.use("/auth", AuthController);
+};
+
+module.exports = routes;
