2632645d26dfa710639fd1f848fd8cd2fff316fc
Gentiana-Idrizi <gentiana.idrizi@student.uni-pr.edu>
2023-05-24 23:58:32 +0200

    Update app.js

--- Delivery-api/app.js
+++ Delivery-api/app.js
@@ -10,7 +10,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(cors({origin: 'http://localhost:3001'}));
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
