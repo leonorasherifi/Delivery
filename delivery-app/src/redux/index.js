ba273ef21b9da6113aeb494450eecb4c394f25b0
Gentiana-Idrizi <gentiana.idrizi@student.uni-pr.edu>
2023-05-24 23:56:57 +0200

    Update index.js

--- delivery-app/src/redux/index.js
+++ delivery-app/src/redux/index.js
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSliceReducer from "./userSlice";
+import productSlideReducer from "./productSlide";
 
 
 
 export const Store = configureStore({
     reducer: {
       user : userSliceReducer,
-      
+      product : productSlideReducer
       
     },
   });
