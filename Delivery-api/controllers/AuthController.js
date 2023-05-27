88d90d099a88f883361c5d852cd72969e06e1cbe
Gentiana-Idrizi <gentiana.idrizi@student.uni-pr.edu>
2023-05-27 23:57:48 +0200

    Update AuthController.js

--- Delivery-api/controllers/AuthController.js
+++ Delivery-api/controllers/AuthController.js
@@ -1,15 +1,67 @@
-const express = require('express');
+const express = require("express");
+const UserDBModel = require("../models/UserModel");
 
 const router = express.Router();
 
-router.post('/signup', async (_req, res, next) => {
-    try {
-        const data = _req.body;
-        console.log('test', data);
-        res.status(200).send({ data: 'Pong!' });
-    } catch (err) {
-        next(err);
-    }
+router.post("/signup", async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const { email } = req.body;
+
+
+    const result = await UserDBModel.findOne({email: email});
+
+    if (result) {
+        res.send({ message: "Email id is already register", alert: false });
+      } else {
+        const data = UserDBModel(req.body);
+        const save = data.save();
+        res.send({ message: "Successfully sign up", alert: true });
+      }
+  } catch (err) {
+    next(err);
+  }
 });
 
+router.post('/login', async (req, res, next) => {
+    try {   
+         console.log(req.body);
+  const { email } = req.body;
+  const result = await UserDBModel.findOne({ email: email });
+
+  
+    if (result) {
+      const dataSend = {
+        _id: result._id,
+        firstName: result.firstName,
+        lastName: result.lastName,
+        email: result.email,
+        image: result.image,
+      };
+      console.log(dataSend);
+      res.send({
+        message: "Login is successfully",
+        alert: true,
+        data: dataSend,
+      });
+    } else {
+      res.send({
+        message: "Email is not available, please sign up",
+        alert: false,
+      });
+    }
+  
+
+    } catch (err) {
+        next(err)
+    }
+})
+
 module.exports = router;
+
+
+
+// //api login
+// app.post("/login", (req, res) => {
+//  
+// });
