820ac06720d10c2618199eea7d8ab99b23f6952c
Gentiana-Idrizi <gentiana.idrizi@student.uni-pr.edu>
2023-05-27 23:58:57 +0200

    Update login.js

--- delivery-app/src/page/login.js
+++ delivery-app/src/page/login.js
@@ -42,7 +42,7 @@ const Login = () => {
     e.preventDefault()
     const {email,password} = data
     if(email && password ){
-      const fetchData = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/login`,{
+      const fetchData = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/auth/login`,{
         method : "POST",
         headers : {
           "content-type" : "application/json"
