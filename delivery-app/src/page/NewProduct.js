405c8fcf7f4080cb23f052d02d88ed07e5a2c7c7
Gentiana-Idrizi <gentiana.idrizi@student.uni-pr.edu>
2023-05-28 12:51:07 +0200

    Update NewProduct.js

--- delivery-app/src/page/NewProduct.js
+++ delivery-app/src/page/NewProduct.js
@@ -43,7 +43,7 @@ const Newproduct = () => {
     const {name,image,category,price} = data
 
     if(name && image && category && price){
-      const fetchData = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/product`,{
+      const fetchData = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/products`,{
         method : "POST",
         headers : {
           "content-type" : "application/json"
