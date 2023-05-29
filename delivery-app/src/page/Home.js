890cad8a515bd97688f0cdc56447f01bdaf3186f
Gentiana-Idrizi <gentiana.idrizi@student.uni-pr.edu>
2023-05-29 20:58:36 +0200

    Update Home.js

--- delivery-app/src/page/Home.js
+++ delivery-app/src/page/Home.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
 import { useSelector } from "react-redux";
 import CardFeature from "../component/CardFeature";
 import HomeCard from "../component/HomeCard";
 import { GrPrevious, GrNext } from "react-icons/gr";
-import FilterProduct from "../component/FilterProduct";
 import AllProduct from "../component/AllProduct";
 
 
