630610bdf97ab7437baf07b76199a07f37b19a7d
gentianamejzini <75703142+gentianamejzini@users.noreply.github.com>
2023-05-30 00:10:02 +0200

    Update HomeCard.js

--- delivery-app/src/component/HomeCard.js
+++ delivery-app/src/component/HomeCard.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const HomeCard = () => {
+const HomeCard = ({ name, image, category, price, loading,id }) => {
   return (
-    <div>HomeCard</div>
-  )
-}
+    <div className="bg-white shadow-md p-2 rounded min-w-[150px]">
+      {name ? (
+        <>
+        <Link to={`/menu/${id}`} onClick={()=>window.scrollTo({top:"0",behavior : "smooth"})} >
+          <div className="w-40 min-h-[150px]">
+            <img src={image} className="h-full w-full" />
+          </div>
+          <h3 className="font-semibold text-slate-600 text-center capitalize text-lg">
+            {name}
+          </h3>
+          <p className="text-center text-slate-500  font-medium">{category}</p>
+          <p className="text-center font-bold">
+            <span className="text-red-500">€</span>
+            <span>{price}</span>
+          </p>
+          </Link>
+        </>
+      ) : (
+        <div className="flex justify-center items-center h-full">
+          <p>{loading}</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default HomeCard
+export default HomeCard;
