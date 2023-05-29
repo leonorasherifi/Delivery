d8ec91117eeed63b0b072bee2c1c4b8a6a990245
gentianamejzini <75703142+gentianamejzini@users.noreply.github.com>
2023-05-30 00:07:03 +0200

    Update FilterProduct.js

--- delivery-app/src/component/FilterProduct.js
+++ delivery-app/src/component/FilterProduct.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { CiForkAndKnife } from "react-icons/ci";
 
-const FilterProduct = () => {
+const FilterProduct = ({category,onClick,isActive}) => {
   return (
-    <div>FilterProduct</div>
-  )
-}
+    <div onClick={onClick}>
+      <div className={`text-3xl p-5  rounded-full cursor-pointer ${isActive ? "bg-red-600 text-white" : "bg-yellow-500"}`}>
+        <CiForkAndKnife />
+      </div>
+      <p className="text-center font-medium my-1 capitalize">{category}</p>
+    </div>
+  );
+};
 
-export default FilterProduct
+export default FilterProduct;
