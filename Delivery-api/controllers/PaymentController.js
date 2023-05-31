a77d8762303d1212d8ac40c1567995a19df73f40
Gentiana Idrizi <93874728+Gentiana-Idrizi@users.noreply.github.com>
2023-05-31 12:46:00 +0200

    Update PaymentController.js

--- Delivery-api/controllers/PaymentController.js
+++ Delivery-api/controllers/PaymentController.js
@@ -1,64 +1,48 @@
 const express = require('express');
+const Stripe = require('stripe');
 
 const router = express.Router();
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
+router.post("/create-checkout-session", async (req, res) => {
+  try {
+    const params = {
+      submit_type: 'pay',
+      mode: "payment",
+      payment_method_types: ['card'],
+      billing_address_collection: "auto",
+      shipping_options: [{ shipping_rate: "shr_1NDD4eB0NPICjzKlSn8SmW7s" }],
+      line_items: req.body.map((item) => {
+        return {
+          price_data: {
+            currency: "inr",
+            product_data: {
+            name: item.name,
+            //images: [item.image]
+            },
+            unit_amount: item.price * 100,
+          },
+          quantity: item.qty
+        };
+      }),
+      success_url: `${process.env.REACT_APP_SERVER_DOMIN}/success`,
+      cancel_url: `${process.env.REACT_APP_SERVER_DOMIN}/cancel`
+    };
 
-// /*****payment getWay */
-// console.log(process.env.STRIPE_SECRET_KEY)
-
-// const stripe  = new Stripe(process.env.STRIPE_SECRET_KEY)
-
-// app.post("/create-checkout-session",async(req,res)=>{
-
-
-// })
+    const session = await stripe.checkout.sessions.create(params);
+    res.status(200).json(session.id);
+  } catch (err) {
+    res.status(err.statusCode || 500).json(err.message);
+  }
+});
 
 router.get('/', async (_req, res, next) => {
-    try {
-            //  try{
-    //   const params = {
-    //       submit_type : 'pay',
-    //       mode : "payment",
-    //       payment_method_types : ['card'],
-    //       billing_address_collection : "auto",
-    //       shipping_options : [{shipping_rate : "shr_1N0qDnSAq8kJSdzMvlVkJdua"}],
-
-    //       line_items : req.body.map((item)=>{
-    //         return{
-    //           price_data : {
-    //             currency : "inr",
-    //             product_data : {
-    //               name : item.name,
-    //               // images : [item.image]
-    //             },
-    //             unit_amount : item.price * 100,
-    //           },
-    //           adjustable_quantity : {
-    //             enabled : true,
-    //             minimum : 1,
-    //           },
-    //           quantity : item.qty
-    //         }
-    //       }),
-
-    //       success_url : `${process.env.FRONTEND_URL}/success`,
-    //       cancel_url : `${process.env.FRONTEND_URL}/cancel`,
-
-    //   }
-
-    //   const session = await stripe.checkout.sessions.create(params)
-    //   // console.log(session)
-    //   res.status(200).json(session.id)
-    //  }
-    //  catch (err){
-    //     res.status(err.statusCode || 500).json(err.message)
-    //  }
-
-        console.log('test');
-        res.status(200).send({ data: 'Payment!' });
-    } catch (err) {
-        next(err);
-    }
+  try {
+    console.log('test');
+    res.status(200).send({ data: 'Payment!' });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
